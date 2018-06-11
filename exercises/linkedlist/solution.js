3e0792a17ee7b7d1b3a0423b8805c890e03167ca
Maneesha <venigallamaneesha24@gmail.com>
2018-06-11 10:58:06 -1100

    linkedlist

---
+++ exercises/linkedlist/solution.js
@@ -0,0 +1,44 @@
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let count = 0;
+    while (this.head) {
+      count++;
+      this.head = this.head.next;
+    }
+    return count;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    if (!this.head) {
+      return null;
+    }
+    let node = this.head;
+    while (node) {
+      if (!node.next) {
+        return node;
+      }
+      node = node.next;
+    }
+  }
+}
+module.exports = { Node, LinkedList };
